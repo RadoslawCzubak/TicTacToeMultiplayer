bbba836dba4a3dc27823155283e116bb51b5dd4e
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2022-10-04 19:24:59 +0200

    fix bug

--- app/src/main/java/pl/rczubak/tictactoemultiplayer/features/tictactoeboard/domain/usecase/MakeMoveUseCase.kt
+++ app/src/main/java/pl/rczubak/tictactoemultiplayer/features/tictactoeboard/domain/usecase/MakeMoveUseCase.kt
@@ -1,7 +1,6 @@
 package pl.rczubak.tictactoemultiplayer.features.tictactoeboard.domain.usecase
 
 import pl.rczubak.tictactoemultiplayer.features.tictactoeboard.domain.GameRepository
-import pl.rczubak.tictactoemultiplayer.features.tictactoeboard.domain.model.GameBoard
 import pl.rczubak.tictactoemultiplayer.features.tictactoeboard.domain.model.GameState
 
 class MakeMoveUseCase(
@@ -11,10 +10,17 @@ class MakeMoveUseCase(
         x: Int,
         y: Int
     ): GameState {
-        val newGameBoard = repository.addMove(x, y)
-        return when(val winPath = newGameBoard.getWinPathIfGameEndElseNull()){
-            null -> GameState.InProgress(newGameBoard)
-            else -> GameState.End(newGameBoard, winner = newGameBoard.nextPlayer, winPath = winPath )
-        }
+        val currentGameBoard = repository.getCurrentGameState()
+        return if (repository.getCurrentGameState().board[x][y] == null) {
+            val newGameBoard = repository.addMove(x, y)
+            when (val winPath = newGameBoard.getWinPathIfGameEndElseNull()) {
+                null -> GameState.InProgress(newGameBoard)
+                else -> GameState.End(
+                    newGameBoard,
+                    winner = newGameBoard.nextPlayer,
+                    winPath = winPath
+                )
+            }
+        } else GameState.InProgress(currentGameBoard)
     }
 }
